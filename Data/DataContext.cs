29cb65e2dfeb427c287b1048b4382b81a80dfaaf
WhiteLightningGun <109026202+WhiteLightningGun@users.noreply.github.com>
2024-01-19 01:22:36 +0000

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    //public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+}   
